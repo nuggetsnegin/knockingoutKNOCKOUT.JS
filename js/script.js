f7ce362d48f73f15d3e33c71f2a03f5401fd0b55
Negin Sauermann <negin.sauermann@campbrain.com>
2020-03-23 12:10:49 -0400

    starting over 😥

--- js/script.js
+++ js/script.js
@@ -1,32 +1,37 @@
-const ClickCounterViewModel = function() {
-  this.numberOfClicks = ko.observable(0);
+// const ClickCounterViewModel = function() {
+//   this.numberOfClicks = ko.observable(0);
 
-  this.registerClick = () => {
-    this.numberOfClicks(this.numberOfClicks() + 1);
-  };
+//   this.registerClick = () => {
+//     this.numberOfClicks(this.numberOfClicks() + 1);
+//   };
 
-  this.resetClicks = () => {
-    this.numberOfClicks(0);
-  };
+//   this.resetClicks = () => {
+//     this.numberOfClicks(0);
+//   };
 
-  this.suprise = ko.pureComputed(function() {
-    return this.numberOfClicks() >= Math.floor(Math.random() * 10) + 3;
-  }, this);
+//   this.suprise = ko.pureComputed(function() {
+//     return this.numberOfClicks() >= Math.floor(Math.random() * 10) + 3;
+//   }, this);
+// };
+
+// ko.applyBindings(new ClickCounterViewModel());
+
+const myViewModel = {
+  name: "Negin"
 };
+ko.applyBindings(myViewModel);
 
-ko.applyBindings(new ClickCounterViewModel());
+// const ViewModel = function(first, last) {
+//   this.firstName = ko.observable(first);
+//   this.lastName = ko.observable(last);
 
-const ViewModel = function(first, last) {
-  this.firstName = ko.observable(first);
-  this.lastName = ko.observable(last);
+//   this.fullName = ko.pureComputed(function() {
+//     // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
+//     return this.firstName() + " " + this.lastName();
+//   }, this);
+// };
 
-  this.fullName = ko.pureComputed(function() {
-    // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
-    return this.firstName() + " " + this.lastName();
-  }, this);
-};
-
-ko.applyBindings(
-  new ViewModel("Planet", "Earth"),
-  document.getElementById("nameInput")
-);
+// ko.applyBindings(
+//   new ViewModel("Planet", "Earth"),
+//   document.getElementById("nameInput")
+// );
